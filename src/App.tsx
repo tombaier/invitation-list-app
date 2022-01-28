0a15bcb06f6efe2ecba249263c07db911e89be0c
Baier,Tom (HR Appr) BIP-DE-I <tom.baier@boehringer-ingelheim.com>
2022-01-28 11:58:19 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List';
+import AddToList from './components/AddToList';
+
+export interface IState {
+  people: {
+    name: string,
+    age: number,
+    url: string,
+    note?: string,
+  }[] 
+}
 
 function App() {
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name:"Justin Timberlake",
+      age: 40,
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf-Y5aVz9ylma2LPo9bCRT2x7KoyPUA2WEfrA-uX1XVpPuEHFV",
+      note: "American singer-songwriter and actor",
+
+    }
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>People Invited to the Meeting</h1>
+      <List people={people}/>
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
---
+++ src/components/AddToList.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import { IState as Props } from '../App';
+
+interface IProps {
+    people: Props['people']
+    setPeople: React.Dispatch<React.SetStateAction<Props ['people']>>
+
+}
+const AddToList: React.FC<IProps> = ({ people, setPeople }) => {
+
+    const [input, setInput] = useState({
+        name: '',
+        age: '',
+        img: '',
+        note: '',
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleClick = (): void => {
+        if(
+            !input.name || !input.age || !input.img
+        ) {
+            return 
+        }
+
+        setPeople([
+            ...people, {
+                name: input.name,
+                age: parseInt(input.age),
+                url: input.img,
+                note: input.note,
+            }
+        ])
+
+        setInput({
+            name: '',
+            age: '',
+            img: '',
+            note: '',
+        })
+    }
+
+    return(
+        <div className='AddToList'>
+            <input 
+                type='text'
+                placeholder='Name' 
+                className='AddToList-input'
+                value={input.name}
+                onChange={handleChange}
+                name='name'
+            />
+
+            <input 
+                type='text' // doesn't work with 'Number'?
+                placeholder='Age' 
+                className='AddToList-input'
+                value={input.age}
+                onChange={handleChange}
+                name='age'
+            />
+
+            <input 
+                type='text'
+                placeholder='Image URL' 
+                className='AddToList-input'
+                value={input.img}
+                onChange={handleChange}  
+                name='img'
+            />
+
+            <textarea
+                placeholder='Note' 
+                className='AddToList-input'
+                value={input.note}
+                onChange={handleChange} //doesn't work with the e for HTMLInputElement
+                // We need 'ChangeEventHandler<HTMLTextAreaElement>' 
+                // instead of 'ChangeEventHandler<HTMLInputElement>'
+                // solved with | -> const handleChange
+                name='note'
+            />
+
+            <button 
+                className='AddToList-btn'
+                onClick={handleClick}
+            >
+                Add to List
+            </button>
+        </div>
+    )
+}
+
+export default AddToList
